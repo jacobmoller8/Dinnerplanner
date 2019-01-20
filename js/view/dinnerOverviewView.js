31ffa6bc4e7d3fbaaebbe4adfbdf51bf5fc75d67
Jacob Moller <jacob.moller8@hotmail.com>
2019-01-20 18:51:58 +0100

    changes J

--- js/view/dinnerOverviewView.js
+++ js/view/dinnerOverviewView.js
@@ -8,7 +8,7 @@ var dinnerOverviewView = function (container, model) {
 
 
 
-    var Test = container.find("#test");
+    var dishesModel = container.find("#dishesModel");
     var menu = model.getFullMenu();
     var dishes = "";
 
@@ -16,13 +16,24 @@ var dinnerOverviewView = function (container, model) {
 
     for (var i = 0; i < menu.length; i++) {
         var dish = model.getDish(menu[i]);
-        dishes += '<div class="col-md-6">' +
+        dishes += '<div class="col-md-2">' +
             '<img src="images/' + dish.image + '" width="100%" height="150px">' +
-            '<button class="btn btn-default btn-block">' + dish.name + '</button>' +
-            '<p align="right">' + 'func for price' + ' SEK </p>' +
-            '</div>';
+            '<button class="btn btn-default btn-block bg-light border-dark">' + dish.name + '</button>' +
+            '<h4 id="dishPrice">100 SEK </h4>' +
+            '</div>'
     }
 
-    Test.html();
+    startRow = '<div class="row justify-content-md-center">'
+
+
+    rightSide = '<div id="totalCost" class="col-md-auto border-left">' +
+        '<h2>Total:</h2>' +
+        '<h3>300 SEK</h3>' +
+        '</div>'
+
+    endRow = '</div>'
+
+
+    dishesModel.html(startRow + dishes + rightSide + endRow);
 
 }
