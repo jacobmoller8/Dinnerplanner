6a935e5507736a6c3ed6e932d2c685857b90e6e7
hugo bergqvist <hugobergqvist@hugos-MBP.lan>
2019-01-20 20:26:39 +0100

    moved row-tag from html file to JS

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -7,6 +7,9 @@ var selectDishView = function (container, model) {
     var menu = model.getFullMenu();
     var dishes = "";
 
+    var row_start = "<div class=" + 'row' + ">"
+    var row_end = "</div>"
+
     for (var i = 0; i < menu.length; i++) {
         var dish = model.getDish(menu[i]);
         dishes += '<div class="container-fluid col-12 col-sm-2 imgCont">' +
@@ -14,5 +17,5 @@ var selectDishView = function (container, model) {
                      '<button class="btn btn-secondary dishBtn">'+ dish.name + '</button>' + '</div>'
         }
 
-        dishSpan.html(dishes);
+        dishSpan.html(row_start + dishes + row_end);
     }
