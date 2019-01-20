dbb32829bce1e2c73b5da897487d3bc46bee5dce
hugo bergqvist <hugobergqvist@hugos-MBP.lan>
2019-01-20 20:20:01 +0100

    Trying to get data into span of select_dish.html

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -1,15 +1,18 @@
-var selectDishView = function(container, model){
+var selectDishView = function (container, model) {
     this.container = container;
 
     var dishSpan = container.find("#dishesSpan");
+    var test = container.find("#TESTER");
+    test.html("<h4>TESTSUCCEDED</h4>");
     var menu = model.getFullMenu();
     dishes = "";
 
     for (var i = 0; i < menu.length; i++) {
         var dish = model.getDish(menu[i]);
-        dishes += '<div class="container-fluid col-12 col-sm-2 imgCont">' + '<img src=images/"' +  dish.image  +'" class="img-fluid foodPic" alt="Responsive image"/>'+ '<button class="btn btn-secondary dishBtn">'+ dish.name + '</button>' + '</div>'
+        dishes += '<div class="container-fluid col-12 col-sm-2 imgCont">' +
+                     '<img src="images/' +  dish.image  +'" class="img-fluid foodPic" alt="Responsive image"/>'+
+                     '<button class="btn btn-secondary dishBtn">'+ dish.name + '</button>' + '</div>'
         }
 
         dishSpan.html(dishes);
-    
     }
