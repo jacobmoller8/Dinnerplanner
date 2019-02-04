896a6a9be165032e0dbb57118f43128dbcfab7e6
Jacob Moller <jacob.moller8@hotmail.com>
2019-02-04 16:11:35 +0100

    mini update DetailsView

--- js/controller/dishSearchController.js
+++ js/controller/dishSearchController.js
@@ -7,7 +7,7 @@ var dishSearchController = function (view, model, app) {
 
     view.container.on('click', '.dishBtn', function () {
         model.setSelectedDishId(this.id);
-        //app.showDishDetailsScreen();
+        app.showDishDetailsScreen();
     });
 
 
